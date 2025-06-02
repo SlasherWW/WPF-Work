1129a5246650e112a1af416c3ce07bac56fbd11b
SlasherWW <166911357+SlasherWW@users.noreply.github.com>
2025-06-02 16:30:51 +0400

    Add files via upload

---
+++ TextBoxes/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TEXTBOXES
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {  
+        Brush backgr;
+        Brush foregr;
+        public MainWindow()
+        {
+            InitializeComponent();
+            backgr = textBox1.Background;
+            foregr = textBox1.Foreground;
+            textBox1.Focus();
+        }
+        private void textBox1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = e.Source as TextBox;
+            tb.Select(tb.Text.Length, 0);
+            tb.Foreground = Brushes.White;
+            tb.Background = Brushes.Green;
+        }
+        private void textBox1_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = e.Source as TextBox;
+            tb.Foreground = foregr;
+            tb.Background = backgr;
+        }
+    }
+}
